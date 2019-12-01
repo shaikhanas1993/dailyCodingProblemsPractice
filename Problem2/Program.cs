720f4d04c6c0843c6b4b83485b46545fbef121d1
anas1993 <shaikhanas1993@gmail.com>
2019-12-01 12:57:53 +0530

    added sln to problem 2 with divide and conquer strategy

--- Problem2/Program.cs
+++ Problem2/Program.cs
@@ -11,6 +11,46 @@ namespace Problem2
 // Follow-up: what if you can't use division?
     class Program
     {
+
+        //Time Complexity of this sln is O(n)
+
+        public  static void calculateProduct2(int[] input)
+        {
+            int n = input.Length;
+            
+            if(n == 1)
+            {
+                return;
+            }
+
+            int[] left = new int[n];
+            int[] right = new int[n];
+            int[] product = new int[n];
+
+
+            left[0] = 1;
+            right[n - 1] = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                left[i] = input[i - 1] * left[i - 1];
+            }
+
+            for (int j = n - 2; j >= 0; j--)
+            {
+                right[j] = input[j + 1] * right[j + 1];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                product[i] = left[i] * right[i];
+            }
+
+
+        }
+
+
+          //Time Complexity of this sln is O(n^2)
         public void calculateProduct(int[] input)
         {
             int[] output = new int[input.Length];
@@ -37,9 +77,9 @@ namespace Problem2
         }
         static void Main(string[] args)
         {
-            int[] arrayOfIntegers = new int[] {3,2,0};
+            int[] arrayOfIntegers = new int[] {1,2,3,4,5};
             Program program = new Program();
-            program.calculateProduct(arrayOfIntegers);
+            calculateProduct2(arrayOfIntegers);
         }
     }
 }
