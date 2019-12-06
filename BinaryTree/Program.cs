cf4a11173edd5dd2a2fe9c9612ce4d6138d71ddb
anas_shaik <anas.shaikh@sundaymobility.com>
2019-12-06 12:57:38 +0530

    added code for binary tree in c#

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BinaryTree
+{
+
+    public class Node
+    {
+        public int data;
+        public Node left,right;
+        public Node(int item)
+        {
+            this.data = item;
+            left = right = null;
+        }
+    }
+
+    public class BinTree
+    {
+        public Node node;
+        public BinTree()
+        {
+            node = null;
+        }
+
+        public BinTree(int key) 
+        { 
+            node = new Node(key); 
+        } 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           BinTree tree = new BinTree();
+           tree.node = new Node(1);
+           tree.node.left = new Node(2);
+           tree.node.right = new Node(3);
+           tree.node.left.left = new Node(4);
+        }
+    }
+}
